0a9c80da3a990483244fa17f4f21871bb81d75c6
hanpil00 <hanpil00@naver.com>
2024-10-11 07:56:27 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main { 
+  public static void main(String[] args) { 
+    String str1 = "|\\_/|";
+    String str2 = "|q p|   /}";
+    String str3 = "( 0 )\"\"\"\\";
+    String str4 = "|\"^\"`    |";
+    String str5 = "||_/=\\\\__|";
+      
+    System.out.println(str1);
+    System.out.println(str2);
+    System.out.println(str3);
+    System.out.println(str4);
+    System.out.println(str5);
+ } 
+}
