85cb86bdb5eb54a403d164e7a1c456ff75ddb4dc
hanpil00 <hanpil00@naver.com>
2024-10-11 07:56:18 +0900

    [Bronze V] Title: 고양이, Time: 92 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main { 
+  public static void main(String[] args) { 
+    String str1 = "\\    /\\";
+    String str2 = " )  ( ')";
+    String str3 = "(  /  )";
+    String str4 = " \\(__)|";
+      
+    System.out.println(str1);
+    System.out.println(str2);
+    System.out.println(str3);
+    System.out.println(str4);
+ } 
+}
